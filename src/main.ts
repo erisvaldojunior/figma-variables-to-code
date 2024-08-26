784cf05a5c3a6056a226b79556c0e507e23dd741
Erisvaldo Junior <erisvaldojunior@gmail.com>
2024-08-26 15:28:17 -0300

    load/save GitHub info

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,35 @@ export default function () {
   figma.ui.postMessage({ dartFile });
   /* Catches event when code copied to clipboard and notify the user */
   figma.ui.onmessage = (message) => {
-	if (message.type === 'code-copied-dart') {
-		figma.notify('Dart code successfully copied to clipboard');
-	}
+  	if (message.type === 'code-copied-dart') {
+	  	figma.notify('Dart code successfully copied to clipboard');
+	  } else if (message.type === 'saveSettings') {
+      // Save settings to clientStorage
+      figma.clientStorage.setAsync('githubUsername', message.username);
+      figma.clientStorage.setAsync('githubToken', message.token);
+      figma.clientStorage.setAsync('githubRepo', message.repo);
+      figma.clientStorage.setAsync('githubBranch', message.branch);
+      figma.clientStorage.setAsync('githubFilePath', message.filePath);
+    } else if (message.type === 'loadSettings') {
+      // Load settings from clientStorage
+      loadSettings();
+    }
   };
+}
+
+async function loadSettings() {
+  const username = await figma.clientStorage.getAsync('githubUsername');
+  const token = await figma.clientStorage.getAsync('githubToken');
+  const repo = await figma.clientStorage.getAsync('githubRepo');
+  const branch = await figma.clientStorage.getAsync('githubBranch');
+  const filePath = await figma.clientStorage.getAsync('githubFilePath');
+  figma.ui.postMessage({
+    type: 'settingsLoaded',
+    username,
+    token,
+    repo,
+    branch,
+    filePath
+  });
+
 }
